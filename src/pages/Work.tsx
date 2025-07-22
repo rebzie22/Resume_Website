adf70a003ab2a60fa4a1ff4694506d30a7803943
rebzie22 <jordanguck22@gmail.com>
2025-07-22 14:57:26 -0500

    Remove unused import from work.tsx

--- src/pages/Work.tsx
+++ src/pages/Work.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import websiteProjectImg from '../assets/websiteproject.png'; // adjust path if needed
 import minesweeperImg from '../assets/minesweeper.png';
 
